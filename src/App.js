729f74f2abfba1ad22cc61a29aa1df855aa46242
Nataliia <Nata13pr@gmail.com>
2023-09-17 11:23:30 +0300

    створила блок фільтрів додати поля для фільтру name, flight_number, date

--- src/App.js
+++ src/App.js
@@ -4,47 +4,54 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-const [totalPagesAmount,setTotalPagesAmount]=useState(1);
-const [launches,setLaunches]=useState([]);
-const [currentPage,setCurrentPage]=useState(1)
+  const [totalPagesAmount, setTotalPagesAmount] = useState(1);
+  const [launches, setLaunches] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(()=>{
-    handlerToDownLoadLauncher(4)  
-    .then((response)=>{
-     setLaunches(response.data.docs);
-     setTotalPagesAmount(response.data.totalPages)
-     setCurrentPage(response.data.page)
-
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-  },[])
-
-  const handlerToDownLoadLauncher=(page)=>{
-   return axios.post('https://api.spacexdata.com/v5/launches/query',{
-      "query": {},
-      "options": {
-        page
-      }
-    })
-    
-  }
- 
-  const handlerAddLaunchesButton=()=>{
-    const newPage = currentPage + 1;
-    handlerToDownLoadLauncher(newPage)
-    .then((response)=>{
-      setLaunches((prev)=>{
-        return [...prev,...response.data.docs]
+  useEffect(() => {
+    handlerToDownLoadLauncher(4)
+      .then((response) => {
+        setLaunches(response.data.docs);
+        setTotalPagesAmount(response.data.totalPages);
+        setCurrentPage(response.data.page);
       })
-    })
-    setCurrentPage(newPage)
-  }
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-  
+  const handlerToDownLoadLauncher = (page) => {
+    return axios.post("https://api.spacexdata.com/v5/launches/query", {
+      query: {},
+      options: {
+        page,
+      },
+    });
+  };
+
+  const handlerAddLaunchesButton = () => {
+    const newPage = currentPage + 1;
+    handlerToDownLoadLauncher(newPage).then((response) => {
+      setLaunches((prev) => {
+        return [...prev, ...response.data.docs];
+      });
+    });
+    setCurrentPage(newPage);
+  };
+
   return (
     <div className="App">
+      <div>
+        <label> Name
+          <input />
+        </label>
+        <label>Flight Number
+          <input />
+        </label>
+        <label> Date
+          <input />
+        </label>
+      </div>
       <List launches={launches} />
       <button onClick={handlerAddLaunchesButton}>Add More</button>
     </div>
