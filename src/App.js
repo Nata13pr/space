a59dd2ed8a0305c5356cc57a63fad0b8f5ea2b17
Nataliia <Nata13pr@gmail.com>
2023-09-17 10:57:53 +0300

    створила сервіс (функцію) для скачування запусків посторінково аргументом вказувати номер сторінки

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
 import List from "./components/List";
+import axios from "axios";
 
 function App() {
 
-
+  const handlerToDownLoadLauncher=(page)=>{
+    axios.post('https://api.spacexdata.com/v5/launches/query',{
+      "query": {},
+      "options": {
+        page
+      }
+    }).then((response)=>{
+console.log(response);
+    })
+  }
+  handlerToDownLoadLauncher(4)
   const launches=['name','age']
   
   return (
